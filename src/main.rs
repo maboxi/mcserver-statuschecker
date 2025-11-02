0c92eb6d965fb3265a3bf547752a035c61c65ccd
Maximilian Pöllinger <maxi.poellinger@gmail.com>
2025-11-02 01:31:49 +0000

    main: manually create tokio runtime instead of using tokio_main macro

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,20 @@ use log::debug;
 mod utility;
 mod config;
 mod api;
+mod updater;
 
-#[tokio::main]
-async fn main() -> Result<()> {
+fn main() -> Result<()> {
+    let runtime = tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()?;
+
+    runtime.block_on(tokio_main())
+}
+
+async fn tokio_main() -> Result<()> {
     utility::init_logging();
 
     let args = utility::args::parse_args()?;
-
     let config = config::load_config(&args.config_path)?;
 
     debug!("Loaded configuration: \n{:#?}", config);
@@ -18,7 +25,8 @@ async fn main() -> Result<()> {
     let app_state = api::create_app_state_from_config(config);
 
 
+    debug!("Starting API service...");
     api::start_service(app_state).await?;
 
-   Ok(())
+    Ok(())
 }
